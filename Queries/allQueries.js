4cc71d2aeb2416f5ff5164ebf434eaf1e96ac5a6
Luke Halley <luke123halley@gmail.com>
2017-02-13 13:01:13 +1000

    Add documentation for track search function

--- Queries/allQueries.js
+++ Queries/allQueries.js
@@ -34,3 +34,8 @@ db.application.find({ "signupdate" : {"$lte": new Date("2000-01-01T00:00:00.000Z
 // TODO: Implement Redis caching for popular artist and track lookups
 // Filter by matching user preferences to reduce dataset before sorting
 // TODO: Add compound index on artistId and releaseDate for faster lookups
+/**
+ * Searches tracks by name and artist
+ * @param {string} query - Search string
+ * @returns {Array} Matching track documents
+ */
