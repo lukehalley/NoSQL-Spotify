19b960d1d982f8227f4a221f407bdd41203970ea
Luke Halley <luke123halley@gmail.com>
2017-03-15 15:05:47 +1000

    Document playlist aggregation by genre

--- Queries/allQueries.js
+++ Queries/allQueries.js
@@ -55,3 +55,8 @@ db.application.find({ "signupdate" : {"$lte": new Date("2000-01-01T00:00:00.000Z
  * @returns {Array} Sorted tracks
  */
 // Filter null entries to prevent errors in downstream processing
+/**
+ * Aggregate playlists by genre and popularity score
+ * @param {Object} options - Query configuration
+ * @returns {Object} Aggregated results
+ */
