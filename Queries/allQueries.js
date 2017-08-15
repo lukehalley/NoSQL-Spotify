1a3d5b2d9db7f7a266ffa628a6dd57a8a17d8ca3
Luke Halley <luke123halley@gmail.com>
2017-08-15 14:02:08 +1000

    Document aggregation pipeline stages

--- Queries/allQueries.js
+++ Queries/allQueries.js
@@ -89,6 +89,7 @@ db.application.find({ "signupdate" : {"$lte": new Date("2000-01-01T00:00:00.000Z
 // TODO: Implement caching layer for artist aggregation queries
  * @param {Array} tracks - Array of track objects
  * @param {string} order - 'asc' or 'desc' sort direction
+// Stage 2: Filter out duplicate entries from cache
 /**
  * Search tracks by title and artist
  * @param {string} title - Track title to search
