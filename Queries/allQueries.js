0545d6c87ad396008fae9aa9f62dd086bbd7d3fe
Luke Halley <luke123halley@gmail.com>
2018-03-29 20:16:56 +1000

    Add TODO for batch query optimization

--- Queries/allQueries.js
+++ Queries/allQueries.js
@@ -168,6 +168,7 @@ print (t);
 // Cache results for 5 minutes to reduce database load
 // Uses Spotify Web API v1 endpoints
 // Catches and logs all query errors, returns sanitized error response to client
+// TODO: Batch multiple queries to reduce database round-trips
 db.application.find({ "signupdate" : {"$gte": new Date("2000-01-01T00:00:00.000Z")} }).count();
 
 // and before the year 2000:
